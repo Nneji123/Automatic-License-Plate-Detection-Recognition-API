6e041f34a281c18f353f9e2b623797382b22fcea
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-16 08:16:44 +0000

    separated api files

---
+++ api/app.py
@@ -0,0 +1,80 @@
+import io
+import os
+import sys
+
+import cv2
+import numpy as np
+import pytesseract
+from fastapi import FastAPI, File, Request, Response, UploadFile
+from fastapi.responses import FileResponse, StreamingResponse
+from PIL import Image
+
+from helpers import get_plate, load_model, preprocess_image
+
+sys.path.append(os.path.abspath(os.path.join("..", "config")))
+
+
+app = FastAPI(
+    title="AVNPR API",
+    description="""Automatic Vehicle Number Plate Recognition API.""",
+)
+
+
+favicon_path = "./images/favicon.ico"
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get("/")
+async def running():
+    note = """
+    AVNPR API 📚
+    Automatic Vehicle License Plate Recognition API!
+    Note: add "/redoc" to get the complete documentation.
+    """
+    return note
+
+
+# endpoint for just enhancing the image
+@app.post("/predict")
+async def predict_plot_image(file: UploadFile = File(...)):
+
+    contents = io.BytesIO(await file.read())
+    file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.imwrite("./images/image.jpg", img)
+    vehicle, LpImg, cor = get_plate("./images/image.jpg")
+    arr = np.array(LpImg[0], dtype=np.float32)
+    pred_img = Image.fromarray((arr * 255).astype(np.uint8)).convert("RGB")
+    pred_img.save("./images/newimage.jpg")
+    return FileResponse("./images/newimage.jpg", media_type="image/jpg")
+
+
+@app.post("/detect")
+async def get_ocr(file: UploadFile = File(...)) -> str:
+
+    contents = io.BytesIO(await file.read())
+    file_bytes = np.asarray(bytearray(contents.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.imwrite("./images/image.jpg", img)
+    vehicle, LpImg, cor = get_plate("./images/image.jpg")
+    value = np.array(LpImg[0], dtype=np.float32)
+    pred_img = Image.fromarray((value * 255).astype(np.uint8)).convert("RGB")
+    pred_img.save("./images/newimage.jpg")
+    image = Image.open("./images/newimage.jpg")
+    # Extracting text from image
+    custom_config = r"-l eng --oem 3 --psm 6"
+    text = pytesseract.image_to_string(image, config=custom_config)
+
+    # Remove symbol if any
+    characters_to_remove = "!()@—*“>+-/,'|£#%$&^_~"
+    new_string = text
+    for character in characters_to_remove:
+        new_string = new_string.replace(character, "")
+
+    # Converting string into list to dislay extracted text in seperate line
+    new_string = new_string.split("\n")
+    return new_string[0]
---
+++ api/helpers.py
@@ -0,0 +1,75 @@
+import glob
+from os.path import basename, splitext
+
+import cv2
+import numpy as np
+import pytesseract
+import tensorflow as tf
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from tensorflow import keras
+from tensorflow.keras.models import model_from_json
+
+from utils import detect_lp
+
+
+def load_model(path):
+    try:
+        path = splitext(path)[0]
+        with open("%s.json" % path, "r") as json_file:
+            model_json = json_file.read()
+        model = model_from_json(model_json, custom_objects={})
+        model.load_weights("%s.h5" % path)
+        print("Model Loaded successfully...")
+        print("Detecting License Plate ... ")
+        return model
+    except Exception as e:
+        print(e)
+
+
+wpod_net_path = "./models/wpod-net.json"
+wpod_net = load_model(wpod_net_path)
+
+
+def preprocess_image(image_path, resize=False):
+    img = cv2.imread(image_path)
+    from PIL import Image
+
+    # img = Image.open(image_path)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img / 255
+    if resize:
+        img = cv2.resize(img, (224, 224))
+    return img
+
+
+def get_plate(image_path, Dmax=608, Dmin=608):
+    vehicle = preprocess_image(image_path)
+    ratio = float(max(vehicle.shape[:2])) / min(vehicle.shape[:2])
+    side = int(ratio * Dmin)
+    bound_dim = min(side, Dmax)
+    _, LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
+    return vehicle, LpImg, cor
+
+
+def get_text_ocr(image_path: str) -> str:
+    vehicle, LpImg, cor = get_plate(image_path)
+    value = np.array(LpImg[0], dtype=np.float32)
+    pred_img = Image.fromarray((value * 255).astype(np.uint8)).convert("RGB")
+    pred_img.save("./images/newimage.jpg")
+    # cv2.imwrite("newimage.jpg", LpImg[0])
+    image = Image.open("./images/newimage.jpg")
+    # Extracting text from image
+    custom_config = r"-l eng --oem 3 --psm 6"
+    text = pytesseract.image_to_string(image, config=custom_config)
+
+    # Remove symbol if any
+    characters_to_remove = "!()@—*“>+-/,'|£#%$&^_~"
+    new_string = text
+    for character in characters_to_remove:
+        new_string = new_string.replace(character, "")
+
+    # Converting string into list to dislay extracted text in seperate line
+    new_string = new_string.split("\n")
+    return new_string[0]
